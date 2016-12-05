a11d8fdd4c42e550736af740b43f84eb567ad1b9
zhangjianghua <xszhangjh@126.com>
2016-12-05 20:35:26 +0800

    second request

--- src/com/atguigu/github/test/GitHubTest.java
+++ src/com/atguigu/github/test/GitHubTest.java
@@ -3,5 +3,6 @@ package com.atguigu.github.test;
 public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("My first request!");
+		System.out.println("My second request!");
 	}
 }

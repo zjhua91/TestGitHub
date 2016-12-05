ad898848b94d54668af0c1baa023e559dcf14a91
zjhua91 <xszhangjh@126.com>
2016-12-05 21:26:27 +0800

    服务器比本地新

--- src/com/atguigu/github/test/GitHubTest.java
+++ src/com/atguigu/github/test/GitHubTest.java
@@ -5,5 +5,6 @@ public class GitHubTest {
 		System.out.println("My first request!");
 		System.out.println("My second request!");
 		System.out.println("收到第一次request！");
+		System.out.println("服务器比本地新！");
 	}
 }

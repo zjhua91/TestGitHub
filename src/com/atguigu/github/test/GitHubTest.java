1ec94da9cd6331d7a418b90d23df94617a54501d
zjhua91 <xszhangjh@126.com>
2016-12-05 20:39:27 +0800

    谢谢

--- src/com/atguigu/github/test/GitHubTest.java
+++ src/com/atguigu/github/test/GitHubTest.java
@@ -4,5 +4,6 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("My first request!");
 		System.out.println("My second request!");
+		System.out.println("收到第一次request！");
 	}
 }
